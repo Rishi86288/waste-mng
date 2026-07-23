7552352b01007fbbaa35863fa5333099d03cdf51
Pallavi Kumari <ecellcipet@gmail.com>
2026-07-24 02:54:40 +0530

    6th commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // यह Webpack को बताएगा कि async_hooks को लोकल फाइल न समझे
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        'async_hooks': 'node:async_hooks',
+      };
+    }
+    return config;
+  },
+};
 
 export default nextConfig;
