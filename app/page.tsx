c8c960d376defa83d4a16b722071bb53d8d1137a
Pallavi Kumari <ecellcipet@gmail.com>
2026-07-24 19:04:10 +0530

    7th commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-[80vh] p-6 text-center">
-      <h1 className="text-3xl font-bold mb-4 text-green-800">कचरे को सही जगह पर लगाएं!</h1>
+      <h1 className="text-3xl font-bold mb-4 text-green-800">please,कचरे को सही जगह पर लगाएं!</h1>
       <p className="text-gray-600 max-w-md mb-8">
         अपने मोबाइल कैमरे से कचरे की तस्वीर लें, AI द्वारा उसकी पहचान करें, सही डिब्बे का सुझाव पाएं और ग्रीन पॉइंट्स जीतें।
       </p>
